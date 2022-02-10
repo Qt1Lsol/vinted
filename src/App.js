f6500792ecd4d65935212427dacf5e2e0bb5c9a6
unknown <qlagache@gmail.com>
2022-02-10 14:26:25 +0100

    all routes are done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Offer from "./pages/Offer";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/offer/:id" element={<Offer />} />
       </Routes>
     </Router>
---
+++ src/pages/Login.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      //   console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <div>En cours de chargement...</div>
+  ) : (
+    <div>
+      {data.offers.map((offer) => {
+        return (
+          <Link key={offer._id} to={`/offer/${offer._id}`}>
+            <div className="card">
+              <h2>{offer.product_name}</h2>
+              <img
+                style={{ height: 150 }}
+                src={offer.product_image.secure_url}
+                alt=""
+              />
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Login;
